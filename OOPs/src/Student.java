b26b641b6cf46dd4604571abac970f1f3a16aec2
Dhiraj Yadav <dhiraj799177@gmail.com>
2026-07-28 11:52:12 +0530

    add classes, objects , constructor of  OOPs

--- OOPs/src/Student.java
+++ OOPs/src/Student.java
@@ -1,2 +1,44 @@
 public class Student {
+    // attributes
+    public int id;
+    public int age ;
+    public String name;
+    public int nos;
+
+    // default constructor // attr.-> garbage
+
+    public Student(){
+        System.out.println("student default ctor Called");
+    }
+// parametrized ctor
+   public Student(int id,int age ,String name , int nos ){
+    System.out.println("student parametrized ctor Called");
+    this.id=id;
+    this.age = age;
+    this.name = name;
+    this.nos = nos;
+}
+// copy ctor
+    public Student(Student srcobj ) { // A -> b
+        System.out.println("student copy ctor Called");
+        this.id = srcobj.id;
+        this.age = srcobj.age;
+        this.name = srcobj.name;
+        this.nos = srcobj.nos;
+    }
+
+    // methods / behaviours
+    public void study(){
+        System.out.println(name + " Studying");
+    }
+    public void sleep(){
+        System.out.println(name + " Sleeping");
+    }
+    public void bunk(){
+        System.out.println(name + " Bunking");
+    }
+
+
+
+
 }
--- OOPs/src/app.java
+++ OOPs/src/app.java
@@ -1,2 +1,40 @@
 public class app {
+    public static void main(String[] args) throws Exception{
+        // default ctor
+//        Student A = new Student();
+//        A.id=1;
+//        A.age=14;
+//        A.name = "neeraj";
+//        A.nos=5;
+//        System.out.println(A.name);
+//        System.out.println(A.id);
+//        System.out.println(A.age);
+//        System.out.println(A.nos);
+//
+//        A.bunk();
+//        A.study();
+//        A.sleep();
+
+
+        // parametrized ctor
+        Student A= new Student(1,18,"Dhiraj",3);
+        System.out.println(A.name);
+        System.out.println(A.id);
+        System.out.println(A.age);
+        System.out.println(A.nos);
+
+        A.bunk();
+        A.study();
+        A.sleep();
+         //copy ctor
+
+//        Student B = new Student(A);
+//        System.out.println(B.name);
+//        System.out.println(B.id);
+//        System.out.println(B.age);
+//        System.out.println(B.nos);
+//
+//        B.sleep();
+
+    }
 }
