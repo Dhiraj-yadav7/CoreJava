4d386c7d8dedd2c2ab5996bce559ec60e153b61a
Dhiraj Yadav <dhiraj799177@gmail.com>
2026-08-06 19:50:08 +0530

    Create a collection framework and add remove, add, removeall, function

--- src/CollectionFramework/main.java
+++ src/CollectionFramework/main.java
@@ -12,5 +12,35 @@ public class main {
        ArrayList<Integer> arr =  new ArrayList<>();
        List<Integer> list = new ArrayList<>();
         Collection<Integer> collection = new ArrayList<>();
+
+        //add
+         list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+        list.add(40);
+        System.out.println(list);
+
+        list.remove(0);
+        System.out.println(list);
+
+        //addAll
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(101);
+        list2.add(102);
+
+        list.addAll(list2);
+        System.out.println(list);
+        list.removeAll(list2);
+        System.out.println(list);
+        System.out.println(list.size());
+
+        System.out.println("printing list 2 ;" + list2);
+        list2.clear();
+        System.out.println(list2.size());
+
+        // I want to traverse list using iterator
+
     }
 }
