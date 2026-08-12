fbea9f42d3533f52e30a73f1a2691f7105c73741
Dhiraj Yadav <dhiraj799177@gmail.com>
2026-08-12 20:48:29 +0530

    add stack queue

--- src/CollectionFramework/main.java
+++ src/CollectionFramework/main.java
@@ -99,6 +99,25 @@ PriorityQueue<Integer> pq = new PriorityQueue<>();
         }
         }
 
+public class Main {
+    public static void main(String[] args) {
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack);
+
+        System.out.println("Top: " + stack.peek());
+
+        System.out.println("Removed: " + stack.pop());
+
+        System.out.println("After pop: " + stack);
+    }
+}
+
 
 
     }
